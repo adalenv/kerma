d8cbfdda90e4f3480ff78d721366a10cc5ad14e8
adalenv <adalenvladi@gmail.com>
2018-07-11 07:23:43 -0400

    fixed

--- run.sh
+++ run.sh
@@ -2,15 +2,14 @@
 nmap_path=`which nmap`
 file_name=`echo $1 | awk -F "/" '{print $1}'`
 echo 'Running nmap'
-$nmap_path -T5 -sT -p 8291 --open $1 -oG - | awk '$4=="Ports:"{print $2}' | awk -F "/" '{print $1}' > tmp/output_`echo $1 | awk -F "/" '{print $1}'`.txt
+#$nmap_path -T5 -sT -p 8291 --open $1 -oG - | awk '$4=="Ports:"{print $2}' | awk -F "/" '{print $1}' > tmp/output_`echo $1 | awk -F "/" '{print $1}'`.txt
+$nmap_path -sP $1 | awk '/is up/ {print up}; {gsub (/\(|\)/,""); up = $NF}'  > tmp/output_`echo $1 | awk -F "/" '{print $1}'`.txt
 
- #file_content=`cat output_$file_name.txt`
- IFS=$'\r\n' GLOBIGNORE='*' command eval  'arr=($(cat tmp/output_$file_name.txt))'
 
-echo 'Nmap finished'
-for i in "${arr[@]}"
-do
-	python3 PoC.py $i
+file="tmp/output_$file_name.txt"
+lines=`cat $file`
+for line in $lines; do
+	echo "-------------------------------------------" >> bambam.txt
+        python3 PoC.py $line  >> bambam.txt
 done
-
 echo "Finish"
