91361dbd0416fff90ff2420a582e856243341177
adalenv <adalenvladi@gmail.com>
2018-07-23 13:57:51 +0200

    check if range is done before

--- run.sh
+++ run.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 nmap_path=`which nmap`
 file_name=`echo $1 | awk -F "/" '{print $1}'`
-echo 'Running nmap'
-#$nmap_path -T5 -sT -p 8291 --open $1 -oG - | awk '$4=="Ports:"{print $2}' | awk -F "/" '{print $1}' > tmp/output_`echo $1 | awk -F "/" '{print $1}'`.txt
+file="tmp/output_$file_name.txt"
+if [ -f $file ]; then
+	echo -e '\033[0;31mThis range is done!\033[0m'
+	while true; do
+		read -p "Continue anyway (y/N) ?   " yn
+		case $yn in
+			[Yy]* ) break ;;
+			    * ) exit  ;;
+		esac
+	done
+fi
+echo -e '\033[0;32mRunning nmap...\033[0m'
+
 $nmap_path -sP $1 | awk '/is up/ {print up}; {gsub (/\(|\)/,""); up = $NF}'  > tmp/output_`echo $1 | awk -F "/" '{print $1}'`.txt
 
-
-file="tmp/output_$file_name.txt"
 lines=`cat $file`
 for line in $lines; do
 	echo "-------------------------------------------" >> bambam.txt
         python3 PoC.py $line  >> bambam.txt
 done
-echo "Finish"
+echo "Done!"
