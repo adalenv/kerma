6c94e70e001e40834edda882baeacce66de32725
adalenv <adalenvladi@gmail.com>
2018-07-23 16:46:24 +0200

    one reg

--- one.sh
+++ one.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-grep -rn  $1  tmp/
+clear
+grep -rn  $1  tmp/ &> /dev/null
 chk=$?
 if [ "$chk" -eq 0 ]; then
 	echo -e '\033[0;31mThis IP is done!\033[0m'
@@ -11,6 +12,7 @@ if [ "$chk" -eq 0 ]; then
 		esac
 	done
 fi
-
+echo $1 >> tmp/one.txt
 python3 PoC.py $1  >> bambam.txt
+tail -n 10 bambam.txt
 echo "Done"
