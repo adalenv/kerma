98a5ab654815de9eadf282dd5af82d5c6bdb2d37
adalenv <adalenvladi@gmail.com>
2018-08-10 12:12:25 +0200

    bug fix on event.sh

--- event.sh
+++ event.sh
@@ -12,15 +12,16 @@ function test0(){
 	#ip=$1
 	#ip=$( zenity --entry --text "Type a range or IP " )
 	ip=$(xclip -out -selection clipboard)
-	lastip=$(head -1 tmp.txt)
+	lastip=$(cat tmp.txt)
 
 	res=$( ./validateIp.sh $ip)
 	if [ $res == "ok" ];then
 		if [ $lastip != $ip ];then
 			echo $ip >> tmp/one.txt
 			python3 PoC.py $ip  >> bambam.txt
-			echo $ip > tmp.txt	
+				
 		fi
+		echo $ip > tmp.txt
 	fi
 	sleep 1
 }
