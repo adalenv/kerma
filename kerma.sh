e7fd22cbef01de3d4333ec23dc46b9b3bb8dd871
adalenv <adalenvladi@gmail.com>
2018-09-03 13:23:10 +0200

    bug fix

--- kerma.sh
+++ kerma.sh
@@ -8,8 +8,14 @@ echo "#                            #"
 echo "#                            #"
 echo "##############################"
 #ip=$1
+if [ -z "$1" ];then
+        ip=$( zenity --entry --text "Type a range or IP " )
+	else
+		ip=$1
+fi
+
 #ip=$( zenity --entry --text "Type a range or IP " )
-ip=$(xclip -out -selection clipboard)
+#ip=$(xclip -out -selection clipboard)
 if [ -z "$ip" ];then
 	echo "You must specify an IP address or a RANGE !"
 	exit 1
